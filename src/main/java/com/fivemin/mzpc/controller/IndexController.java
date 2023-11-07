77eaa386ec2f0910479311b6d47478ce743b313d
Kim tae-seop <kts3445@naver.com>
2023-11-07 17:58:49 +0900

    Refactor: 초기 화면
    1. url 변경
    2. return 변경

--- src/main/java/com/fivemin/mzpc/controller/IndexController.java
+++ src/main/java/com/fivemin/mzpc/controller/IndexController.java
@@ -2,13 +2,14 @@ package com.fivemin.mzpc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
+    @RequestMapping("/")
     public String index() {
 
-        return "redirect:/login";
+        return "login";
     }
 }
