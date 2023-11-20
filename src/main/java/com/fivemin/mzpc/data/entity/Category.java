ebb21b9b773f56ba4e9aa698d15b1a89cab7c8dc
Kim tae-seop <kts3445@naver.com>
2023-11-20 14:57:52 +0900

    Chore : Entity 수정
    Entity 파일 수정

--- src/main/java/com/fivemin/mzpc/data/entity/Category.java
+++ src/main/java/com/fivemin/mzpc/data/entity/Category.java
@@ -19,13 +19,14 @@ public class Category{
     //카테고리 일련번호
     @Column(name = "category_code",nullable = false, unique = true)
     private String code;
-    
+
     //카테고리 이름
+    // 유니크 속성 추가하기
     @Column(name = "category_name",nullable = false, length = 30)
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "admin_idx",nullable = false)
-    private Admin admin;
+    @JoinColumn(name = "store_idx",nullable = false)
+    private Store store;
 
 }
