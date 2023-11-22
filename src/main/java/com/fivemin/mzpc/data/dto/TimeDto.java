c91e2b63828aec88cb66eabb12669e1fdf350d79
Kim tae-seop <kts3445@naver.com>
2023-11-22 14:28:47 +0900

    Remove .DS_Store files

---
+++ src/main/java/com/fivemin/mzpc/data/dto/TimeDto.java
@@ -0,0 +1,22 @@
+package com.fivemin.mzpc.data.dto;
+
+import com.fivemin.mzpc.data.entity.Times;
+import lombok.*;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TimeDto {
+
+    private String code;
+    private String name;
+    private Integer price;
+    private Duration addTime;
+    private boolean save;
+    private TimeDto timeDto;
+}
--- src/main/java/com/fivemin/mzpc/data/repository/TimesRepository.java
+++ src/main/java/com/fivemin/mzpc/data/repository/TimesRepository.java
@@ -1,7 +1,13 @@
 package com.fivemin.mzpc.data.repository;
 
+import com.fivemin.mzpc.data.dto.TimeDto;
+import com.fivemin.mzpc.data.entity.Store;
 import com.fivemin.mzpc.data.entity.Times;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TimesRepository extends JpaRepository<Times, Long> {
+    List<Times> findByStore(Store store);
+    Times findByCode(String code);
 }
