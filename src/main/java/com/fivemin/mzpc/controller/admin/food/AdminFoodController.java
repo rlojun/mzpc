f482055f3690f9ccf2728c181329d2e4503422be
rlojun <cjun0117@hanmail.net>
2023-12-14 16:22:29 +0900

    Merge

--- src/main/java/com/fivemin/mzpc/controller/admin/food/AdminFoodController.java
+++ src/main/java/com/fivemin/mzpc/controller/admin/food/AdminFoodController.java
@@ -16,21 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-
-/*
-- 기능
-카테고리 별 음식 상품 목록
-    화면에 갯수 넘어가면 스크롤
-음식 상품 상세보기
-    주문시 재고량 빠지는 기능
-    이건 member쪽일듯 - 원식
-음식 상품 수정
-    사진 및 재고량 등록
-음식 상품 삭제
-음식 상품 추가
- */
 @Controller
 @Slf4j
 @RequestMapping("/admin/{storeCode}") //관리자 pk
@@ -62,9 +50,9 @@ public class AdminFoodController {
         model.addAttribute("foods",foodDtos);
 
         String resultView = "";
-        if (topping == false) {
+        if (!topping) {
             resultView = "admin/food/listFood";
-        } else if (topping == true) {
+        } else {
             resultView = "admin/food/topping/toppingList";
 
         }
@@ -72,15 +60,15 @@ public class AdminFoodController {
     }
 
     @GetMapping(value = "/addFoodForm")
-    public String addFoodForm(@PathVariable String storeCode, @RequestParam boolean topping ,Model model){
+    public String addFoodForm(@PathVariable String storeCode, @RequestParam boolean topping , Model model){
         List<CategoryDto> categoryDtos = adminCategoryService.getListCategory(storeCode);
         model.addAttribute("storeCode", storeCode);
         model.addAttribute("categories",categoryDtos);
 
         String resultView = "";
-        if (topping == false) {
+        if (!topping) {
             resultView = "admin/food/addFoodForm";
-        } else if (topping == true) {
+        } else {
             resultView = "admin/food/topping/addToppingForm";
 
         }
@@ -106,52 +94,13 @@ public class AdminFoodController {
         model.addAttribute("storeCode",storeCode);
 
         String resultView = "";
-        if (topping == false) {
+        if (!topping) {
             resultView = "detailFood";
-        } else if (topping == true) {
+        } else if (topping) {
             resultView = "topping/detailTopping";
         }
 
         return "admin/food/"+resultView;
     }
 
-
-    //    // 카테고리별 음상 상품 리스트
-//    @GetMapping("/{categoryId}")
-//    public String listFoodCategory() {
-//        return "";
-//    }
-//
-//    // 즐겨찾기 리스트
-//    @GetMapping("/favorites")
-//    public String listFoodFavorites() {
-//        return "";
-//    }
-//
-//    // 토핑 리스트
-//    @GetMapping("/topping")
-//    public String listTopping(){return "";}
-//    /*
-//    addFoodForm
-//    (음식 상품 추가 화면 이동 메서드)
-//
-//    addFood
-//    (음식 상품 추가 등록 확인 메서드)
-//
-//    detailFood
-//    (음식 상품 상세 화면 이동 메서드)
-//
-//    returnFoodList
-//    (음식 상세 화면에서 음식 상품 목록으로 돌아가기 메서드)     // > 삭제 예정
-//
-//    modifyFoodForm
-//    (음식 상품 수정 화면으로 이동하는 메서드)
-//
-//    modifyFood
-//    (음식 상품 수정 확인 메서드)
-//
-//    deleteFood
-//    (음식 상품 삭제 확인 메서드)
-//
-//     */
 }
