499b68f70c3cf85c2dccea2f1717e3a44c8f4a79
rlojun <cjun0117@hanmail.net>
2023-12-18 20:28:32 +0900

    Refactor : 주문 알람 수정 및 Cors 추가
    배포하면서 cors 에러가 떠서 추가

---
+++ src/main/java/com/fivemin/mzpc/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.fivemin.mzpc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept")
+                .maxAge(3600);
+    }
+
+}
